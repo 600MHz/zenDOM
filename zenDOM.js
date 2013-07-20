c1443759879e718e6c44e986c008d8c70f3b42ac
Kevin Huang <600ghz@gmail.com>
2013-07-20 17:15:08 +0800

    Add "close to" operator
    Now the operator "<" is defined as "close to", it must follow an
    element, which means pull level up to this element.
    The element can be omitted tag name which is followed "<".

--- zenDOM.js
+++ zenDOM.js
@@ -2,6 +2,7 @@
  * zenDOM
  * Create dom objects or HTML code by javascript using emmet-like syntax
  *
+ * authour: Kevin Huang <600ghz@gmail.com>
  *
  *
  * element:
@@ -10,114 +11,179 @@
  * structure:
  * element>element+element^element<element
  *
- * return: object with method:
- * getDOM(): get all DOM tree
+ * return: documentFragment object with method:
  * getHTML(): get generated HTML
  * appendTo(domObj): append all element generated to domObj as children.
  *
- * authour: Kevin Huang <600ghz@gmail.com>
+ * Usage:
+ * var str = 'div#space1.box1>ul+li{text1}+li{text2}^^p{demo string}>span<#space1+div';
+ * var dom = zen(str);
+ * var nodes = dom.childNodes;
+ * var html = dom.getHTML();
+ * var nodes = dom.appendTo(document.body);
+ *
+ * **NOTE** in javascript, you should use "\\" to generate one backslash.
+ * E.g.,
+ * var str = 'div[attr1=value\\ 1 attr2=value} attr3=mark_\\]_and_\\\\_in_value]{text, mark\\}, and 3   spaces}';
+ * var html = zen(str).getHTML();
+ * html should be:
+ * <div attr1="value 1" attr2="value}" attr3="mark_]_and_\_in_value">text, mark }, and 3&nbsp;&nbsp; spaces</div>
+ *
  *
  * License: MIT
  */
 
-var zenDOM = function(template) {
-	if (!template) throw "Do not pass an empty string to me"
-	var dce = function(t) { return document.createElement(t); },
-		dtn = function(t) { return document.createTextNode(t); },
-		escapeBackSlashes = function(unesc, str) {
-			var re = new RegExp('\\\\([' + unesc.replace(/([\]\}\!\?\^\\\*\(\)\-\+\=\<\>\/])/g, '\\$1') + '])', 'g');
-			return str.replace(re, '$1');
+(function(window) {
+	var zen = function(template) {
+		if (!template) throw "Do not pass an empty string to me";
+		var escapeBackSlashes = function(unesc, str) {
+				var re = new RegExp('\\\\([' + unesc.replace(/([\]\}\!\?\^\\\*\(\)\-\+\=\<\>\/])/g, '\\$1') + '])', 'g');
+				return str.replace(re, '$1');
+			};
+		var 
+			//unitReg = /\s*([<>\^\+])\s*((?:[^<>\^\+]*?(?:\[(?:\\\]|[^\]])*\])*?(?:{(?:\\}|[^\}])*})*?)+)/g,
+			//Regular expression above does not work in IE
+			unitReg = /\s*([<>\^\+])\s*((?:[^<>\^\+\{\[]*(?:\[(?:\\\]|[^\]])*\])*(?:{(?:\\}|[^\}])*})*)+)/g,
+			elementReg = /((^|#|\.)(\w+)|\[(?:\\\]|[^\]])+\]|\{(?:\\\}|[^\}])+\})+?/g,
+			attrReg = /([^\s=]+)(=((?:\\\s|[^\s])*))?/g;
+
+		var dom = document.createDocumentFragment();
+		var unit, element, rootElement, op, elementString, undef;
+		var createElement = function(elementString) {
+			var part, element, attrStr, attr, classNames = [], id, text, textNode;
+
+			while(null !== (part = elementReg.exec(elementString))) {
+				switch (true) {
+					case part[2] === undef: //[] or {} part
+						if (part[1].substr(0, 1) === '{' && part[1].substr(-1) === '}') { //Text node
+							text = escapeBackSlashes('}', part[1].substr(1, part[1].length - 2));
+							text = text.replace(/  /g, '\u00A0\u00A0');
+							text = text.replace(/\\n/g, '\u000A');
+							textNode = document.createTextNode(text);
+						}
+						if (!textNode && part[1].substr(0, 1) === '[' && part[1].substr(-1) === ']') { //attribute 
+							attrStr = part[1].substr(1, part[1].length - 2);
+							while(null !== (attr = attrReg.exec(attrStr))) { //please ensure tag write at head of unit
+								var tmp = escapeBackSlashes(' ]', attr[3] || '');
+								if (/^[a-z_]/gi.test(attr[1])) element.setAttribute(attr[1], tmp);
+								else throw "Error for \"" + elementString + "\", Attribute of element must start with letters or underline";
+							}
+						}
+						break;
+					case part[2] === '': //tag
+						element = document.createElement(part[3]);
+						break;
+					case part[2] === '.': //class name
+						classNames.push(part[3]);
+						break;
+					case part[2] === '#': //id
+						id = part[3];
+						break;
+				}
+			}
+			if (element) {
+				if (classNames.length > 0) element.className = classNames.join('');
+				if (id) element.id = id;
+				if (textNode) element.appendChild(textNode);
+				return element;
+			} else {
+				return textNode;
+			}
 		};
-	var unitReg = /\s*([<>\^\+])\s*((?:[^<>\^\+]*?(?:\[(?:\\\]|[^\]])*\])*?(?:{(?:\\}|[^\}])*})*?)+)/g,
-		elementReg = /((^|#|\.)(\w+)|\[(?:\\\]|[^\]])+\]|\{(?:\\\}|[^\}])+\})+?/g,
-		attrReg = /([^\s=]+)(=((?:\\\s|[^\s])*))?/g;
 
-	var dom = dce('div');
-	var unit, rootElement, op, elementString, undef;
-	var createElement = function(elementString) {
-		var element, attrStr, attr, className, id, text, textNode;
+		rootElement = dom;
+		while(null !== (unit = unitReg.exec('>' + template))) {
+			op = unit[1];
+			elementString = unit[2];
+			if (elementString == '') {
+				if (op == '^') {
+					rootElement = (rootElement.parentElement || rootElement.parentNode) || rootElement;
+					continue;
+				} else {
+					throw "missing element string near: \"" + template.substr(unitReg.lastIndex - 5 || 0, 9) + "\" in string \"" + template + "\"";
+				}
+			}
 
-		while(null !== (part = elementReg.exec(elementString))) {
-			switch (true) {
-				case part[2] === undef: //[] or {} part
-					if (part[1].substr(0, 1) === '{' && part[1].substr(-1) === '}') { //Text node
-						text = escapeBackSlashes('}', part[1].substr(1, part[1].length - 2));
-						text = text.replace(/  /g, '\u00A0\u00A0');
-						text = text.replace(/\\n/g, '\u000A');
-						textNode = dtn(text);
-					}
-					if (!textNode && part[1].substr(0, 1) === '[' && part[1].substr(-1) === ']') { //attribute 
-						attrStr = part[1].substr(1, part[1].length - 2);
-						while(null !== (attr = attrReg.exec(attrStr))) { //please ensure tag write at head of unit
-							var tmp = escapeBackSlashes(' ]', attr[3] || '');
-							if (/^[a-z_]/gi.test(attr[1])) element.setAttribute(attr[1], tmp);
-							else throw "Error for \"" + elementString + "\", Attribute of element must start with letters or underline";
+			if (op == '<') {
+				rootElement = (function(tmpRoot, elmtStr) {
+					var isTarget, part, attrStr, attr;
+					while (tmpRoot) {
+						if (tmpRoot.nodeType != 1) {
+							tmpRoot = (tmpRoot.parentElement || tmpRoot.parentNode) || null;
+							continue;
+						}
+						isTarget = true;
+						while(null !== (part = elementReg.exec(elmtStr))) { //make sure that is html element not string node
+							switch (true) {
+								case part[2] === undef: //[] or {} part, ignore {}
+									if (part[1].substr(0, 1) === '[' && part[1].substr(-1) === ']') { //attribute 
+										attrStr = part[1].substr(1, part[1].length - 2);
+										while(null !== (attr = attrReg.exec(attrStr))) { //please ensure tag write at head of unit
+											var value = escapeBackSlashes(' ]', attr[3] || '');
+											if (value == '') {
+												isTarget = isTarget && tmpRoot.hasAttribute(attr[1]);
+											} else {
+												isTarget = isTarget && tmpRoot.hasAttribute(attr[1]) && tmpRoot.getAttribute(attr[1]) == value;
+											}
+										}
+									}
+									break;
+								case part[2] === '': //tag
+									isTarget = isTarget && tmpRoot.tagName == part[3].toUpperCase();
+									break;
+								case part[2] === '.': //class name
+									isTarget = isTarget && tmpRoot.className.split(/\s+/g).indexOf(part[3]) !== -1;
+									break;
+								case part[2] === '#': //id
+									isTarget = isTarget && tmpRoot.id == part[3];
+									break;
+							}
+						}
+						if (isTarget) {
+							break;
+						} else {
+							tmpRoot = (tmpRoot.parentElement || tmpRoot.parentNode) || null;
 						}
 					}
+
+					return tmpRoot;
+				})(rootElement, elementString);
+				if (rootElement === null) throw "Cannot find parent for \"" + elementString + "\"";
+				else continue;
+			}
+			element = createElement(elementString);
+			if (!element) continue;
+			switch (op) {
+				case '>':
+					rootElement.appendChild(element);
 					break;
-				case part[2] === '': //tag
-					element = dce(part[3]);
+				case '+':
+					(rootElement.parentElement || rootElement.parentNode).appendChild(element);
 					break;
-				case part[2] === '.': //class name
-					className = part[3];
-					break;
-				case part[2] === '#': //id
-					id = part[3];
+				case '<':
+				case '^':
+					rootElement = (rootElement.parentElement || rootElement.parentNode) || rootElement;
+					rootElement = (rootElement.parentElement || rootElement.parentNode) || rootElement;
+					rootElement.appendChild(element);
 					break;
 			}
+			rootElement = element;
 		}
-		if (element) {
-			if (className) element.className = className;
-			if (id) element.id = id;
-			if (textNode) element.appendChild(textNode);
-			return element;
-		} else {
-			return textNode;
-		}
+
+		rootElement = null;
+
+		dom.getHTML = function() {
+			var tmpElement = document.createElement('div');
+			tmpElement.appendChild(dom.cloneNode(true));
+			return tmpElement.innerHTML;
+		};
+		dom.appendTo = function(node) {
+			return node.appendChild(dom);
+		};
+
+		return dom;
 	};
 
-	rootElement = dom;
-	while(null !== (unit = unitReg.exec('>' + template))) {
-		op = unit[1];
-		elementString = unit[2];
-		if (elementString =='') {
-			if (op == '<' || op == '^') rootElement = rootElement.parentElement || rootElement;
-			else throw "missing element string near: " + template.substr(unitReg.lastIndex - 5 || 0, 9);
-			continue;
-		}
-
-		element = createElement(elementString);
-		if (!element) continue;
-		switch (op) {
-			case '>':
-				rootElement.appendChild(element);
-				break;
-			case '+':
-				rootElement.parentElement.appendChild(element);
-				break;
-			case '<':
-			case '^':
-				rootElement = rootElement.parentElement || rootElement;
-				rootElement = rootElement.parentElement || rootElement;
-				rootElement.appendChild(element);
-				break;
-		}
-		rootElement = element;
-	}
-
-	rootElement = null;
-	return {
-		getDOM: function() {
-			return dom.cloneNode(true).childNodes;
-		},
-		getHTML: function() {
-			return dom.innerHTML;
-		},
-		appendTo: function(domObject) {
-			var node, i, l = dom.childNodes.length;
-			if (!!domObject && domObject instanceof HTMLElement && l) for (i = 0; i < l; i++) {
-				domObject.appendChild(dom.childNodes[i].cloneNode(true));
-			}
-		}
-	};
-};
+	if('zen' in window) throw 'window.zen exists!';
+	else window.zen = zen;
+})(window);
